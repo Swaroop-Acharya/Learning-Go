96455d6a771ce14892043dcd50c689a37b32c554
Swaroop-Acharya <swaroopacharya935@gmail.com>
2025-10-02 21:33:59 +0530

    naked return

--- hello-world.go
+++ hello-world.go
@@ -11,9 +11,17 @@ func swap(x, y string) (string, string){
   return y,x;
 }
 
+// naked return
+func addNsub(x,y int)(add,sub int){
+	add= x+y;
+	sub= x-y;
+	return;
+}
+
 func main()  {
 	fmt.Println(add(1,2))
 	a,b:= swap("hellow","world");
 	fmt.Println(a,b);
+	fmt.Println(addNsub(2,2));
 }
 
