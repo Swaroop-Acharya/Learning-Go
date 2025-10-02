4c110d94c912c32aefb92212c9d8779665c3b74c
Swaroop-Acharya <swaroopacharya935@gmail.com>
2025-10-02 21:21:25 +0530

    multi return values

--- hello-world.go
+++ hello-world.go
@@ -2,7 +2,18 @@ package main
 
 import "fmt"
 
+func add(x int, y int) int {
+  return x+y;
+}
+
+// return multiple return results
+func swap(x, y string) (string, string){
+  return y,x;
+}
+
 func main()  {
-	fmt.Println("hello worl")	
+	fmt.Println(add(1,2))
+	a,b:= swap("hellow","world");
+	fmt.Println(a,b);
 }
 
