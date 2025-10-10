ae15213ab2b305cccf6639ccc39f0c41be1fdf71
Swaroop-Acharya <swaroopacharya935@gmail.com>
2025-10-10 13:01:48 +0530

    range

---
+++ Basics/range.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func rangeIntro() {
+	var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+
+	for i, v := range pow {
+		fmt.Printf("2**%d = %d\n", i, v)
+	}
+}
+
+func rangeContinued() {
+	pow := make([]int, 10)
+
+	for i := range pow {
+		pow[i] = 1 << uint(i)
+	}
+
+	for _,v:= range pow{
+		fmt.Printf("%d\n",v)
+	}
+}
+
+func main() {
+	rangeIntro()
+	rangeContinued()
+}
